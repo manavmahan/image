0b8964b2aba1f28fa94dbf196fec0183a39dc510
Manav Mahan Singh <manav.mahan.singh@live.in>
2022-09-29 09:42:33 +0200

    Update image.js

--- image.js
+++ image.js
@@ -1,9 +1,8 @@
 for (let i = 0; i < 100; i++) { 
-  src = $"https://github.com/manavmahan/image/blob/main/pngs/crop_{i}.png?raw=true"
+  src = $`https://github.com/manavmahan/image/blob/main/pngs/crop_{i}.png?raw=true`
   img = document.createElement('img');
 
   img.src = src;
   document.body.appendChild(img);
 }
-
-Plotly.newPlot('container', plotData);
+// Plotly.newPlot('container', plotData);
