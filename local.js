8c9e95e7307978a01085aceb3c5b0bc050932feb
Manav Mahan Singh <manav.mahan.singh@live.in>
2022-10-03 11:58:03 +0200

    Create local.js

---
+++ local.js
@@ -0,0 +1,12 @@
+function Confidence95(numbers) {
+	var sum = listSum(numbers);
+	var mean = sum/numbers.length;
+	var sqerrs = numbers.map(function(n){ return (n - mean)*(n - mean); });
+	var std = Math.sqrt(listSum(sqerrs)/numbers.length);
+	var interval = 1.959964*std/Math.sqrt(numbers.length);
+	return {
+		mean: mean,
+		std: std,
+		interval: interval
+	};
+};
